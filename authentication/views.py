11d0f7dff239f36dfe2c4875b37a899a225b8b69
Axel S <97108262+axel-schl@users.noreply.github.com>
2022-01-13 19:31:14 -0300

    auth

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.dispatch import receiver
+from django_rest_passwordreset.signals import reset_password_token_created
+from .serializers import UserSerializer
 
-# Create your views here.
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data.get('email', None)
+        password = request.data.get('password', None)
+        user = authenticate(email=email, password=password)
+
+     
+        if user:
+            login(request, user)
+            return Response(UserSerializer(user).data,status=status.HTTP_200_OK)
+
+        
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class LogoutView(APIView):
+    def post(self, request):
+        logout(request)
+
+      
+        return Response(status=status.HTTP_200_OK)
+
+class SignupView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+
+@receiver(reset_password_token_created)
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+   
+    print(f"\nRecover your password '{reset_password_token.user.email}' using this token: '{reset_password_token.key}' from API http://localhost:8000/api/auth/reset/confirm/."")
--- book_store/settings.py
+++ book_store/settings.py
@@ -38,11 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'corsheaders',
     'rest_framework',
+    'django_rest_passwordreset',
+    
     'authentication'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CORS_ORIGIN_WHITELIST = ["http://localhost:3000"]
+CORS_ALLOW_CREDENTIALS = True
